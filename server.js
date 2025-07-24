6b7c62ab9de847af92d200d65b006fde6429a844
InbalMary <inbalby10@gmail.com>
2025-07-24 10:09:07 +0300

    provide an API for Bugs CRUDL

--- server.js
+++ server.js
@@ -1,4 +1,57 @@
 import express from 'express' 
+
+import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
+
 const app = express() 
+app.use(express.static('public'))
+
+app.get('/api/bug', (req, res) => {
+	bugService.query()
+        .then(bugs => res.send(bugs))
+})
+
+app.get('/api/bug/save', (req, res) => {
+	const { title, description, severity, createdAt, _id } = req.query
+	const bugToSave = {
+        _id,
+		title,
+        description,
+		severity: +severity,
+        createdAt,
+	}
+
+    bugService.save(bugToSave)
+        .then(savedBug => res.send(savedBug))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(400).send(err)
+        })
+})
+
+app.get('/api/bug/:id', (req, res) => {
+	const bugId = req.params.id
+
+	bugService.getById(bugId)
+        .then(bug => res.send(bug))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(400).send(err)
+        })
+})
+
+app.get('/api/bug/:id/remove', (req, res) => {
+	const bugId = req.params.id
+
+    bugService.remove(bugId)
+        .then(() => res.send(`bug ${bugId} deleted`))
+        .catch(err => {
+            loggerService.error(err)
+            res.status(400).send(err)
+        })
+})
+
 app.get('/', (req, res) => res.send('Hello there')) 
-app.listen(3030, () => console.log('Server ready at port 3030'))
+
+const port = 3030
+app.listen(port, () => loggerService.info(`Server listening on port http://127.0.0.1:${port}/`))
