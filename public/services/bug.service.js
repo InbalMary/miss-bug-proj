73120e9d050773363980ae0eec4102a5ef7c1c8c
InbalMary <inbalby10@gmail.com>
2025-07-24 11:03:06 +0300

    refactor bugService to use API instead of localStorage

---
+++ public/services/bug.service.js
@@ -0,0 +1,50 @@
+
+const BASE_URL = '/api/bug'
+
+export const bugService = {
+    query,
+    getById,
+    save,
+    remove,
+    getDefaultFilter
+}
+
+function query(filterBy) {
+    return axios.get(BASE_URL)
+        .then(res => res.data)
+        .then(bugs => {
+
+            if (filterBy.txt) {
+                const regExp = new RegExp(filterBy.txt, 'i')
+                bugs = bugs.filter(bug => regExp.test(bug.title))
+            }
+
+            if (filterBy.minSeverity) {
+                bugs = bugs.filter(bug => bug.severity >= filterBy.minSeverity)
+            }
+
+            return bugs
+        })
+}
+
+function getById(bugId) {
+    return axios.get(`${BASE_URL}/${bugId}`)
+        .then(res => res.data)
+}
+
+function remove(bugId) {
+    return axios.get(`${BASE_URL}/${bugId}/remove`)
+        .then(res => res.data)
+}
+
+function save(bug) {
+    var queryStr = `/save?vendor=${bug.vendor}&speed=${bug.speed}`
+    if (bug._id) queryStr += `&_id=${bug._id}`
+
+    return axios.get(BASE_URL + queryStr)
+        .then(res => res.data)
+}
+
+function getDefaultFilter() {
+    return { txt: '', minSeverity: 0 }
+}
